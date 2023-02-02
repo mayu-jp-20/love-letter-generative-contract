42f037ef5c0af002c40153d697aed1e192af3347
mayu-jp-20 <yoshidamayu1221@gmail.com>
2023-02-02 17:14:06 +0900

    add mint nft function

--- contracts/generate-nft.sol
+++ contracts/generate-nft.sol
@@ -5,21 +5,60 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "hardhat/console.sol";
+import {Base64} from "./libraries/Base64.sol";
 
 contract GenerateNFT is ERC721URIStorage {
-
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721 ("LoveLetterNFT", "LL") {
+    constructor() ERC721("LoveLetterNFT", "LL") {
         console.log("This is my NFT contract.");
     }
 
     function generateNFT() public {
         uint256 newItemId = _tokenIds.current();
+        string
+            memory baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+        string memory customWord = "konnnichiwa!";
+        string memory finalSvg = string(
+            abi.encodePacked(baseSvg, customWord, "</text></svg>")
+        );
+
+        console.log("\n--------------------");
+        console.log(finalSvg);
+        console.log("--------------------\n");
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                    // NFTのタイトルを生成される言葉（例: GrandCuteBird）に設定します。
+                    "Love Letter Generative NFT",
+                    '", "description": "A Love Letter Collection using ChatGPT.", "image": "data:image/svg+xml;base64,',
+                    //  data:image/svg+xml;base64 を追加し、SVG を base64 でエンコードした結果を追加します。
+                    Base64.encode(bytes(finalSvg)),
+                    '"}'
+                    )
+                )
+            )
+        );
+
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        console.log("\n----- Token URI ----");
+        console.log(finalTokenUri);
+        console.log("--------------------\n");
+
         _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiRXBpY05mdENyZWF0b3IiLAogICAgImRlc2NyaXB0aW9uIjogIlRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStSWEJwWTA1bWRFTnlaV0YwYjNJOEwzUmxlSFErQ2p3dmMzWm5QZz09Igp9");
-        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+        _setTokenURI(newItemId, finalTokenUri);
+        console.log(
+            "An NFT w/ ID %s has been minted to %s",
+            newItemId,
+            msg.sender
+        );
         _tokenIds.increment();
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,7 @@ const main = async () => {
   const nftContractFactory = await hre.ethers.getContractFactory("GenerateNFT");
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
   let txn = await nftContract.generateNFT();
   await txn.wait();
   console.log("Minted NFT #1");
